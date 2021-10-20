85e85c1987651baff5f881bc39304799069cadf1
Eduardo Aguilar <eaguilar92105@yahoo.com>
2021-10-19 21:19:33 -0700

    created env variables for guild and channel

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 require('dotenv').config();
 
+const guildId = process.env.GUILD_ID;
+const channelId = process.env.CHANNEL_ID;
+
 client.once('ready', () => {
-    console.log("BOOTING UP! 100%, :robot: I am now ready to configure!");
+  console.log("BOOTING UP! 100%, 🤖 I am now ready to configure!");
 });
 
 client.on('message', msg => {
-    console.log(msg);
+  console.log(msg);
+  if (msg.guild.id === guildId && msg.channel.id === channelId) {
+    
+  }
 });
 
 client.login(process.env.BOT_TOKEN); 
