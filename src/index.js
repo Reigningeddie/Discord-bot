91414f9ca02a084913251cb26f968779524c9f53
Eduardo Aguilar <eaguilar92105@yahoo.com>
2021-10-19 18:59:34 -0700

    changed console.log

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+require('dotenv').config();
 
 client.once('ready', () => {
-    console.log("Ready!");
+    console.log("Beeb boop! I am ready!");
 });
 
 client.login(process.env.BOT_TOKEN);
