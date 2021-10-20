a665d13406cb9208d3f930617457ff7d944a8dd4
Eduardo Aguilar <eaguilar92105@yahoo.com>
2021-10-19 19:09:34 -0700

    updated discord.js version

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 require('dotenv').config();
 
 client.once('ready', () => {
-    console.log("Beeb boop! I am ready!");
+    console.log("BOOTING UP! 100%, I am now ready to configure!");
 });
 
 client.login(process.env.BOT_TOKEN);
